4cf7c594ac0fe88ffc00fe3cde129ecc08a29df5
Juha Paananen <juha.paananen@gmail.com>
2012-01-16 22:38:59 +0200

    simplify

--- test/Reactive/BaconTest.hs
+++ test/Reactive/BaconTest.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, FunctionalDependencies #-}
-
 module Reactive.BaconTest(baconTests) where
 
 import Test.HUnit
@@ -25,133 +23,134 @@ baconTests = TestList $ takeWhileTest : filterTest : mapTest
 concatTests = [
   eventTest "concatE with cold observable"
   (concatE [1, 2] [3, 4])
-  [n 1, n 2, n 3, n 4, e]
+  [1, 2, 3, 4]
   ,eventTest "concatE with hot observable"
   (concatE (timed [(0, 1), (1, 2)]) (timed [(0, 3), (1, 4)]))
-  [n 1, n 2, n 3, n 4, e]
+  [1, 2, 3, 4]
   ]
 
 repeatTest = eventTest "repeat repeats indefinitely"
   (takeE 5 $ repeatE [1, 2])
-  [n 1, n 2, n 1, n 2, n 1, e]
+  [1, 2, 1, 2, 1]
 
 laterTest = eventTest "later returns single element later"
   (laterE (units 1) "lol")
-  [n "lol", e]
+  ["lol"]
 
 delayTests = [
   eventTest "delay returns same elements"
     (delayE (units 1) [1, 2, 3])
-    [n 1, n 2, n 3, e]
+    [1, 2, 3]
   ,eventTest "elements are delayed"
     (takeUntilE (delayE (units 2) $ timed [(0, 1), (1, 2), (2, 3)]) (later 4 ["stop"]))
-    [n 1, n 2, e]
+    [1, 2]
   ]
 
 throttleTests = [
   publishedEventTest "throttle throttles"
     (timed [(0, 1), (1, 2), (3, 3), (1, 4), (1, 5)])
     (throttleE (units 2))
-    [n 2, n 5, e] 
+    [2, 5] 
   ]
 
 periodicTest = eventTest "periodic repeats single event periodically"
   (takeE 2 $ periodicallyE (units 1) "lol")
-  [n "lol", n "lol", e]
+  ["lol", "lol"]
 
 mergeTests = [
   eventTest "mergeE with cold observable" 
     (mergeE [1, 2] [3, 4]) 
-    ([n 1, n 2, n 3, n 4, e])
+    ([1, 2, 3, 4])
   ,eventTest "mergeE with hot observable" 
     (mergeE (timed [(0, "1"), (1, "2")]) (timed [(2, "3"), (1, "4")])) 
-    ([n "1", n "2", n "3", n "4", e])
+    (["1", "2", "3", "4"])
   ]
 
 timedTest = eventTest "timed source delivers" 
   (timed [(0, "a"), (1, "b"), (0, "c")]) 
-  [n "a", n "b", n "c", e]
+  ["a", "b", "c"]
 
 takeUntilTests = [ 
   eventTest "takeUntil with bounded input"
     (takeUntilE (timed [(0, "a"), (2, "b")]) (timed [(1, "stop")]))
-    [n "a", e]
+    ["a"]
   ,eventTest "takeUntil with endless input"
     (takeUntilE (periodic 2 "a") (periodic 5 "stop")) 
-    [n "a", n "a", e]
+    ["a", "a"]
   ]
 
-takeWhileTest = eventTest "takeWhileE takes while condition is true" 
+takeWhileTest = eventTest "takeWhileE takes while conditiois true" 
   (takeWhileE (<3) [1, 2, 3, 1]) 
-  [n 1, n 2, e]
+  [1, 2]
 
 filterTest = eventTest "filterE filters" 
-  (filterE (<3) [1, 2, 3, 1]) ([n 1, n 2, n 1, e])
+  (filterE (<3) [1, 2, 3, 1]) ([1, 2, 1])
 
-mapTest = eventTest "mapE maps" (mapE (+1) [1, 2]) ([n 2, n 3, e])
+mapTest = eventTest "mapE maps" (mapE (+1) [1, 2]) ([2, 3])
 
-scanTest = eventTest "scanE scans" (scanE (+) 0 [1, 2]) ([n 1, n 3, e])
+scanTest = eventTest "scanE scans" (scanE (+) 0 [1, 2]) ([1, 3])
 
-combineLatestTest = eventTest "combineLatest combines" (combineLatestE (timed [(0, "a1")]) (timed [(1, "b1"), (1, "b2")])) [n ("a1", "b1"), n ("a1", "b2"), e]
+combineLatestTest = eventTest "combineLatest combines" 
+  (combineLatestE (timed [(0, "a1")]) (timed [(1, "b1"), (1, "b2")])) 
+  [("a1", "b1"), ("a1", "b2")]
 
 zipTests = [
   eventTest "zipE zips lists"
     (zipE [1,2,3] [4,5,6]) 
-    [n (1,4), n (2,5), n (3,6), e]
+    [(1,4), (2,5), (3,6)]
   ,eventTest "zipE zips lists with hot observables"
     (zipE [1,2,3] (timed [(0, 4), (1, 5), (1, 6)])) 
-    [n (1,4), n (2,5), n (3,6), e]
+    [(1,4), (2,5), (3,6)]
   ]
 
 monadTests = [
   eventTest ">>= collects events from cold observable with hot sub-observables"
     (obs [1, 2, 3] >>= \n -> timed [(n, n)])
-    [n 1, n 2, n 3, e]
+    [1, 2, 3]
   ,eventTest ">>= collects events from simultaneously returning substreams"
     ((obs [1, 2, 3]) >>= (later 2))
     (UnOrdered [1, 2, 3])
   ,eventTest ">>= collects events from cold sub-observables"
     ((obs [1, 2, 3]) >>= return)
-    [n 1, n 2, n 3, e]
+    [1, 2, 3]
   ,eventTest ">>= collects events from hot observable with hot sub-observables"
     ((timed [(0, 1), (1, 2), (1, 3)]) >>= (later 2))
-    [n 1, n 2, n 3, e]
+    [1, 2, 3]
   ]
 
 switchTests = [ 
   eventTest "switch with cold observables is like >>="
     ([1, 2, 3] `switchE` return)
-    [n 1, n 2, n 3, e]
+    [1, 2, 3]
   ,eventTest "switch with cold observables & hot subs is empty"
     ([1, 2, 3] `switchE` (later 1))
-    [e]
-  ,publishedEventTest "switch switches between sub-observables on each new main event"
+    []
+  ,publishedEventTest "switch switches betweesub-observables on each new main event"
     (timed [(0, "a"), (1, "b"), (3, "c")])
     (\src -> src `switchE` (later 2))
-    [n "b", n "c", e]
+    ["b", "c"]
   ,eventTest "doesn't work well for cold main observable, because is based on takeUntilE"
     ((timed [(0, "a"), (1, "b"), (3, "c")]) `switchE` (later 2))
-    [e]
+    []
   ]
 
 takeTests = [
-  eventTest "takeE takes N first events" (takeE 3 [1, 2, 3, 1]) ([n 1, n 2, n 3, e])
-  ,eventTest "takeE ends if source ends" (takeE 3 [1, 2]) ([n 1, n 2, e])
+  eventTest "takeE takes N first events" (takeE 3 [1, 2, 3, 1]) ([1, 2, 3])
+  ,eventTest "takeE ends if source ends" (takeE 3 [1, 2]) ([1, 2])
   ]
 
 publishTests = [
   publishedEventTest "publish produces same results for hot observable"
     (timed [(1, "a"), (2, "b")])
     id
-    [n "a", n "b", e]
+    ["a", "b"]
   ,publishedEventTest "publish consumes a cold observable immediately"
     ([1, 2, 3] <++> later 1 4)
     id
-    [n 4, e]
+    [4]
   ]
 
 n = Next
-e = End
 
 delayMs = 10
 
@@ -173,15 +172,16 @@ later delay value = laterE (units delay) value
 
 units delay = milliseconds $ delay * delayMs
 
-class EventSpec a s | s -> a where
-  verifyEvents :: s -> [Event a] -> Assertion
+class EventSpec s where
+  verifyEvents :: Ord a => Eq a => Show a => s a -> [Event a] -> Assertion
 
-instance (Show a, Eq a) => (EventSpec a) [Event a] where
-  verifyEvents expected actual = assertEqual "incorrect events" expected actual
+instance EventSpec [] where
+  verifyEvents expected actual = assertEqual "incorrect events" expectedEvents actual
+    where expectedEvents = map Next expected ++ [End]
 
 data UnOrdered a = UnOrdered [a]
 
-instance (Show a, Eq a, Ord a) => (EventSpec a) (UnOrdered a) where
+instance EventSpec (UnOrdered) where
   verifyEvents (UnOrdered expected) actualEvents = do
       let actualItems = actualEvents >>= item
       when ((S.fromList actualItems) /= (S.fromList expected)) $ assertEqual "incorrect events" expected actualItems
@@ -196,7 +196,7 @@ publishedEventTest label observable modifier spec = TestLabel label $ TestCase 
   verifyObservable (modifier published) spec
   dispose
 
-eventTest :: EventSpec a sp => Source s => Show a => Eq a => String -> s a -> sp -> Test
+eventTest :: EventSpec sp => Source s => Ord a => Show a => Eq a => String -> s a -> sp a -> Test
 eventTest label observable spec = TestLabel label $ TestCase $ do
   verifyObservable observable spec
 
